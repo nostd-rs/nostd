ac28f8af6f308afa4ed5d362d3cdd5e012cac758
Jeeyong Um <conr2d@proton.me>
2024-12-04 16:52:21 +0900

    feat: Make `io` feature opt-in by default

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -14,18 +14,8 @@ mod impls;
 mod traits;
 mod util;
 
-#[cfg(not(feature = "std"))]
 pub use buffered::{BufReader, BufWriter, LineWriter};
-#[cfg(not(feature = "std"))]
 pub use cursor::Cursor;
-#[cfg(not(feature = "std"))]
 pub use error::{Error, ErrorKind, Result};
-#[cfg(not(feature = "std"))]
 pub use traits::{BufRead, Bytes, Chain, Read, Seek, SeekFrom, Take, Write};
-#[cfg(not(feature = "std"))]
 pub use util::copy;
-
-#[cfg(feature = "std")]
-pub use std::io::{
-    BufRead, Bytes, Chain, Cursor, Error, ErrorKind, Read, Result, Seek, SeekFrom, Take, Write,
-};
