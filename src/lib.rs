11b922658a58f87944d7da1d911664ace9f638aa
Jeeyong Um <conr2d@proton.me>
2025-01-01 23:34:25 +0800

    docs: Update README.md

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,51 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! `nostd` provides essential `std`-like types in `no_std` environments.
+//!
+//! This crate re-exports types from the `alloc` and `core` crates under the
+//! familiar `std` path while also offering alternative implementations for
+//! types not available in those crates.
+//!
+//! `nostd` aims to help port code written for `std` to `no_std` with minimal
+//! changes, often requiring only a replacement of `std::` with `nostd::`.
+//!
+//! However, please note that a successful build does not guarantee that the
+//! code will work as expected. Exercise caution and thoroughly test your
+//! application before using it in production.
+//!
+//! ## Features
+//!
+//! - `std::prelude` emulation
+//! ```rs
+//! use nostd::prelude::*;
+//! // Now you can access `Box`, `String`, `Vec`, etc.
+//! ```
+//! - `std::io` emulation by [`core2`] (feature: `io`)
+//! - `std::collections::hash_(map|set)` emulation by [`hashbrown`]
+//! (feature: `hashbrown`)
+//!
+//! [`core2`]: https://crates.io/crates/core2
+//! [`hashbrown`]: https://crates.io/crates/hashbrown
+//!
+//! ## Usage
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! nostd = "0.1"
+//! ```
+//!
+//! By default, `nostd` is intended to be used in `no_std` environment. To use
+//! it in `std` environment, enable the `std` feature:
+//!
+//! ```toml
+//! [features]
+//! default = ["std"]
+//! std = ["nostd/std"]
+//! ```
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "alloc")]
